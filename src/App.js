bbfa13a113f7d119bec86f1b94bfed4de0fe48b8
Vilius Paliokas <vilpal@bitbucket.org>
2019-12-07 21:55:20 +0200

    Add muiTheme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
 import App from './containers/App' 
+import { createMuiTheme, ThemeProvider  } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+		primary: 
+		{
+			main: "#ff9e80",	
+		},
+		secondary: {
+			main: "#fbe9e7",
+		},	
+  },
+});
+
+
 function app() {
   return (
-    <App/>
+		<ThemeProvider theme={theme}>
+    	<App/>
+		</ThemeProvider>
   );
 }
 
