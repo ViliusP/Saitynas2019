ec372d5b095eb17fb61fa90b944fa20597affbb7
Vilius Paliokas <vilpal@bitbucket.org>
2019-12-07 20:35:42 +0200

    Redirecting non-authenticated user

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,27 +1,60 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import NotFoundPage from "../NotFoundPage/index";
 import LoggedUserLayout from "../LoggedUserLayout/index";
 import SignIn from "../SignIn";
+import Hompage from "../Homepage";
 
-const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
-	Layout === undefined ?
-	//eslint-disable-next-line
-	  Layout = props => (<React.Fragment>{props.children}</React.Fragment>) : null,
-	<Route {...rest} render={props => (
-	  <Layout>
-		<Component {...props} />
-	  </Layout>
-	)} />
-  );
+import JwtDecode from 'jwt-decode';
+
+
+function isJwtValid() {
+	let token = localStorage.getItem("token");
+
+	console.log("Token: ");
+	console.log(token);
+
+	if(token !== null) { 
+		var decoded = JwtDecode(token);
+		console.log(decoded);
+		return true;
+	}
+	return false;
+}
+
 
 export default function App() {
+
+
+const AppRoute = ({ islogged: IsLogged, component: Component, layout: Layout, ...rest }) => (
+	Layout === undefined
+		? //eslint-disable-next-line
+			(Layout = props => <React.Fragment>{props.children}</React.Fragment>)
+		: null,
+	isJwtValid() ? (
+		<Route
+			{...rest}
+			render={props => (
+				<Layout>
+					<Component {...props} />
+				</Layout> 
+			)}
+		/>
+	) : (<Redirect to='/login' />)
+);
+	
   return (
     <div>
       <BrowserRouter>
         <Switch>
-					<AppRoute exact path="/login" component={SignIn} />
-          <AppRoute layout={LoggedUserLayout}  component={NotFoundPage} />
+          <Route exact path="/login" component={SignIn} />
+           <AppRoute
+            exact
+            path="/home"
+            layout={LoggedUserLayout}
+						component={Hompage}
+          />
+          <AppRoute layout={LoggedUserLayout} component={NotFoundPage} />}
         </Switch>
       </BrowserRouter>
     </div>
