446c59d2a702c99591c67837d650d89199357248
Vilius Paliokas <vilpal@bitbucket.org>
2019-12-08 23:51:58 +0200

    Add error handling \SearchTrips

--- src/containers/SearchTrips/index.js
+++ src/containers/SearchTrips/index.js
@@ -5,7 +5,9 @@ import { instanceOf } from "prop-types";
 
 export default function SearchTrips() {
   const [tripsData, setData] = useState([]);
-  const [test, setTest] = useState(0);
+  const [error, setError] = useState("");
+
+  const [test] = useState(0);
 
   let history = useHistory();
   useEffect(() => {
@@ -13,6 +15,7 @@ export default function SearchTrips() {
   }, [test]);
 
   const fetchTrips = () => {
+    setError("");
     const options = {
       method: "GET",
       credentials: "same-origin",
@@ -23,15 +26,13 @@ export default function SearchTrips() {
     fetch("http://localhost:8080/trips", options)
       .then(
         result => {
-          console.log("REsults: ");
-          console.log(result);
           if (result.status === 401) {
             localStorage.clear();
             history.push("/login");
           } else if (result.status > 402 && result.status < 500) {
             console.log("400> <500");
           } else if (result.status >= 500) {
-            console.log("Internal server error");
+            setError("Internal server error");
           } else {
             return result.json();
           }
@@ -45,30 +46,37 @@ export default function SearchTrips() {
         }
       )
       .then(res => {
+        if (res === undefined) {
+          setData([]);
+          return;
+        }
         setData(res);
-        console.log("Success");
-        console.log(res);
       });
   };
-
   return (
     <div>
-      {tripsData.map(trip => (
-        <SearchTripTemplate
-          key={trip.tripID}
-          tripFirstCity={trip.departure_city.name}
-					tripLastCity={trip.destination_city.name}
-					cost = {trip.cost}
-					space = {trip.space}
-					info = {trip.info}
-					postDate={trip.creation_date}
-					departureDate={trip.departure_date}
-					userFirstName={trip.user.first_name}
-					userLastName={trip.user.last_name}
-					phoneNumber = {trip.user.phone_number}
-					photoURL = {trip.user.photo_URL}
-				/>
-      ))}
+      {error === "" ? (
+        <div>
+          {tripsData.map(trip => (
+            <SearchTripTemplate
+              key={trip.tripID}
+              tripFirstCity={trip.departure_city.name}
+              tripLastCity={trip.destination_city.name}
+              cost={trip.cost}
+              space={trip.space}
+              info={trip.info}
+              postDate={trip.creation_date}
+              departureDate={trip.departure_date}
+              userFirstName={trip.user.first_name}
+              userLastName={trip.user.last_name}
+              phoneNumber={trip.user.phone_number}
+              photoURL={trip.user.photo_URL}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>{error}</div>
+      )}
     </div>
   );
 }
